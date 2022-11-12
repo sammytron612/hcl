44624844458b1702a44a9e1cc30bdc2cbffc8158
Sammytron612 <60606374+sammytron612@users.noreply.github.com>
2022-11-12 15:37:04 +0000

    Update mainStats.blade.php

--- resources/views/layouts/mainStats.blade.php
+++ resources/views/layouts/mainStats.blade.php
@@ -8,10 +8,8 @@
 
     <link rel="icon" href="{{ asset('/css/favicon.png') }}" type="image/x-icon"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
    <script src="https://unpkg.com/vue"></script> 
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
 
@@ -31,12 +29,9 @@
         width: calc(100% - 53px);
         margin-left: -9px;
       }
-
       .chatmsg {
         margin-top: 5px;
       }
-
-
       .chat-container {
         margin-top: 15px;
         margin-bottom: 15px;
@@ -45,11 +40,6 @@
           -webkit-animation: fadein 2s; /* Safari and Chrome */
           -o-animation: fadein 2s; /* Opera */
       }
-
-
-
-
-
       @keyframes fadein {
           from {
               opacity: 0;
